35249bd16c4bfc4a5ead0751e9c4aa2d18e45bd6
Zerosource0 <marcjesse92@hotmail.com>
2014-11-12 09:21:59 +0100

    update 1
    Added dictionary.txt file;
    Added Load and Save classes in HelpClasses;

---
+++ HelpClasses/Load.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid.HelpClasses;
+
+import java.io.File;
+import java.util.ArrayList;
+import solid.HelpClasses.FileHandler;
+/**
+ *
+ * @author marcj_000
+ */
+public class Load {
+    
+    public Load(String filePath){
+        
+        FileHandler fileHandler = new FileHandler();
+        
+        ArrayList<String> loadedText = new ArrayList<String>();
+        
+        loadedText = fileHandler.load(filePath);
+        
+    
+        
+    }
+}
